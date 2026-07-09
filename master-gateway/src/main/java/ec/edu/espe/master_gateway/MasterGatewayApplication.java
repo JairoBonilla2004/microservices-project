f6406b7e02835f51969bbbd2e739610edf31e3e9
JairoBonilla2004 <jsbonilla2@espe.edu.ec>
2026-07-08 20:29:39 -0500

    feat: initial Spring Boot project

---
+++ master-gateway/src/main/java/ec/edu/espe/master_gateway/MasterGatewayApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.master_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterGatewayApplication.class, args);
+	}
+
+}
