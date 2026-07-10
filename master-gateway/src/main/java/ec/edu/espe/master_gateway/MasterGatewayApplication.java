d7d377a400732776ad6244cb6320286aad17d75f
JairoBonilla2004 <jsbonilla2@espe.edu.ec>
2026-07-09 19:40:13 -0500

    feat: update main application bootstrap class

--- master-gateway/src/main/java/ec/edu/espe/master_gateway/MasterGatewayApplication.java
+++ master-gateway/src/main/java/ec/edu/espe/master_gateway/MasterGatewayApplication.java
@@ -2,12 +2,25 @@ package ec.edu.espe.master_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+/**
+ * Punto de entrada de la aplicación Master Gateway.
+ *
+ * <p>Inicializa el contexto de Spring Boot con escaneo automático de
+ * propiedades de configuración, actuando como el gateway central para
+ * la autenticación, autorización y enrutamiento de microservicios en
+ * el sistema.</p>
+ *
+ * @author Jairo Bonilla
+ * @author Reishel Tipan
+ * @author Julio Viche
+ */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MasterGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MasterGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MasterGatewayApplication.class, args);
+    }
 }
